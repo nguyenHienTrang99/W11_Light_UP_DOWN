9b4223ecbc5e9e94f3e50565483d94f110cf7e80
nguyenHienTrang99 <115915656+nguyenHienTrang99@users.noreply.github.com>
2022-11-28 12:34:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ let serial;
 let bright = 0; 
 let dark, light; 
 
+// Kết nối từ Arduino qua P5JS 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   dark = color(0);
   light = color(255, 0 , 0);
   serial = new p5.SerialPort();
-  serial.open('COM4');
+  serial.open('COM4'); // Cổng nối
 }
+
 
 function drawGradient(c1, c2) {
   noFill();
@@ -21,18 +23,23 @@ function drawGradient(c1, c2) {
 }
 
 function draw() {
+  // Biến này để tạo nhiều sao
   let hcn = 1.0;  
   let hcd = 0.1; 
+  // Tạo bản đồ sáng tối 
   drawGradient(dark, light);
+  // Tạo thanh di chuyển
   stroke(255);
   strokeWeight(3);
   noFill();
   ellipse(mouseX, mouseY, 10, 10);
+  // Tạo mặt trời
   push();
   translate(width * 0.5, height * 0.75);
   rotate(frameCount / 50.0);
   star(0, 0, 20, 100, 20);
   pop();
+  // Tạo sao
   for(var i = 0 ; i < 12 ; i++){
     push();
     translate(width * hcn, height * hcd);
@@ -42,6 +49,7 @@ function draw() {
     hcn -= 0.17 ; 
   } 
 } 
+
 
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
@@ -58,9 +66,11 @@ function star(x, y, radius1, radius2, npoints) {
   endShape(CLOSE);
 }
 
+// Ánh xạ ánh sáng theo hướng dọc 
 function mouseDragged(){
-  bright = floor(map(mouseY, 0, 512, 0, 255));
+  bright = floor(map(mouseY, 0, windowHeight, 0, 255));
   bright = constrain(bright, 0, 255);
+  // Serial nhan tin hieu va phat anh sang o cap do tuong ung
   serial.write(bright);
   console.log(bright);
 }
